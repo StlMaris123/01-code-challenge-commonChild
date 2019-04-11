d3df44e2bbde91b131e44eb0c2d7374930bf8a3c
StlMaris123 <stellamarisnjage@gmail.com>
2019-04-11 20:50:27 +0300

    common child solution

---
+++ common_child_solution.rb
@@ -0,0 +1,9 @@
+def common_child(str1, str2)
+	arr1 = str1.split("")
+	arr2 = str2.split("")
+	(arr1 & arr2).join('')
+	# (arr1 & arr2).size
+end
+
+puts common_child('ABCD', 'ABBD')
+puts common_child('SALLY', 'HARRY')
